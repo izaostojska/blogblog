f4cce97c3135e17082d5c84dc328684f02d238ab
izabelqa <131399703+izabelqa@users.noreply.github.com>
2024-06-18 17:21:53 +0200

    Add files via upload

---
+++ controllers/comments_controller.rb
@@ -0,0 +1,34 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
+  before_action :set_comment, only: [:destroy]
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to @post, notice: 'Comment was successfully created.'
+    else
+      redirect_to @post, alert: 'Failed to create comment.'
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
---
+++ controllers/posts_controller.rb
@@ -0,0 +1,80 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    if params[:search].present?
+      search_term = "%#{params[:search]}%"
+      if user_signed_in?
+        @posts = Post.where("title LIKE :search OR description LIKE :search OR id IN (:ids)", search: search_term, ids: Post.joins(:tags).where(tags: { name: params[:search] }).pluck(:id))
+                     .distinct
+      else
+        @posts = Post.where(public: true).where("title LIKE :search OR description LIKE :search OR id IN (:ids)", search: search_term, ids: Post.joins(:tags).where(tags: { name: params[:search] }).pluck(:id))
+                     .distinct
+      end
+    else
+      @posts = if user_signed_in?
+                 Post.all
+               else
+                 Post.where(public: true)
+               end
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    tag_names = params[:post][:tag_names].split(',')
+    tag_names.each do |tag_name|
+      tag = Tag.find_or_create_by(name: tag_name.strip)
+      @post.tags << tag
+    end
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      # Aktualizacja tagów na podstawie przekazanych tagów
+      tag_names = params[:post][:tag_names].split(',').map(&:strip)
+      @post.tags = tag_names.map { |name| Tag.find_or_create_by(name: name) }
+
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    print("metdoa")
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description, :image, :public)
+  end
+end
